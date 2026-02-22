c81b94496c1019d49f7bf2e701db9f0c57524f5a
Helen Hui <helenhui@dynamic-oit-ip4-wifirestricted03-10-49-71-12.princeton.edu>
2026-02-22 16:02:41 -0500

    done

--- client.go
+++ client.go
@@ -1,31 +1,62 @@
 /*****************************************************************************
  * client.go
- * Name:
- * NetId:
+ * Name: Yuanning Hui
+ * NetId: hh9077
  *****************************************************************************/
 
- package main
+package main
 
- import (
-	 "log"
-	 "os"
- )
- 
- const SEND_BUFFER_SIZE = 2048
- 
- /* TODO: client()
-  * Open socket and send message from stdin.
-  */
- func client(server_ip string, server_port string) {
- 
- }
- 
- // Main parses command-line arguments and calls client function
- func main() {
-	 if len(os.Args) != 3 {
-		 log.Fatal("Usage: ./client [server IP] [server port] < [message file]")
-	 }
-	 server_ip := os.Args[1]
-	 server_port := os.Args[2]
-	 client(server_ip, server_port)
- }
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const SEND_BUFFER_SIZE = 2048
+
+/* client()
+ * Open socket and send message from stdin.
+ */
+func client(server_ip string, server_port string) {
+	// connect to the server via TCP
+	conn, err := net.Dial("tcp", server_ip+":"+server_port)
+	if err != nil {
+		log.Fatal("Failed to connect to server: ", err)
+	}
+	defer conn.Close()
+
+	// read from stdin in chunks and send to server
+	buf := make([]byte, SEND_BUFFER_SIZE) // make buffer for reading from stdin with size SEND_BUFFER_SIZE 
+	// such that we can handle messages larger than the buffer size by sending in multiple chunks
+	for { // infinite for loop until error  or EOF
+		n, err := os.Stdin.Read(buf)
+		if n > 0 { // if we read some bytes, send them to the server
+			// handle partial sends: keep sending until all n bytes are sent
+			sent := 0 // number of bytes sent so far
+			for sent < n {
+				written, werr := conn.Write(buf[sent:n]) // conn.Write may write fewer than n bytes, so we need to check how many bytes were actually written
+				if werr != nil {
+					log.Fatal("Failed to send data: ", werr)
+				}
+				sent += written
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal("Failed to read from stdin: ", err)
+		}
+	}
+}
+
+// Main parses command-line arguments and calls client function
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("Usage: ./client [server IP] [server port] < [message file]")
+	}
+	server_ip := os.Args[1]
+	server_port := os.Args[2]
+	client(server_ip, server_port)
+}
--- server.go
+++ server.go
@@ -1,31 +1,70 @@
 /*****************************************************************************
  * server.go
- * Name:
- * NetId:
+ * Name: Yuanning Hui
+ * NetId: hh9077
  *****************************************************************************/
 
- package main
+package main
 
- import (
-	 "log"
-	 "os"
- )
- 
- const RECV_BUFFER_SIZE = 2048
- 
- /* TODO: server()
-  * Open socket and wait for client to connect
-  * Print received message to stdout
-  */
- func server(server_port string) {
- 
- }
- 
- // Main parses command-line arguments and calls server function
- func main() {
-	 if len(os.Args) != 2 {
-		 log.Fatal("Usage: ./server [server port]")
-	 }
-	 server_port := os.Args[1]
-	 server(server_port)
- }
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const RECV_BUFFER_SIZE = 2048
+
+/* server()
+ * Open socket and wait for client to connect
+ * Print received message to stdout
+ */
+func server(server_port string) {
+	// listen on the specified port 
+	ln, err := net.Listen("tcp", ":"+server_port)
+	if err != nil {
+		log.Fatal("Failed to listen on port: ", err)
+	}
+	defer ln.Close()
+
+	
+	for { // infinite loop to accept clients 
+		conn, err := ln.Accept() // accept a client connection
+		if err != nil { 
+			// accept errors are client-connection related; log and continue (not fatal to the server) to next iteration to accept next client
+			log.Print("Failed to accept connection: ", err)
+			continue
+		}
+
+		// read all data from this client and print it to stdout; symmetric to client 
+		buf := make([]byte, RECV_BUFFER_SIZE)
+		for {
+			n, err := conn.Read(buf)
+			if n > 0 {
+				_, werr := os.Stdout.Write(buf[:n])
+				if werr != nil {
+					log.Print("Failed to write to stdout: ", werr)
+				}
+			}
+			if err == io.EOF {
+				// client closed the connection so we go to next iteration 
+				break
+			}
+			if err != nil {
+				// client connection error
+				log.Print("Error reading from connection: ", err)
+				break
+			}
+		}
+		conn.Close()
+	}
+}
+
+// Main parses command-line arguments and calls server function
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: ./server [server port]")
+	}
+	server_port := os.Args[1]
+	server(server_port)
+}
